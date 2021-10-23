9427a79efff8d9ade2c0c80ad90781bef16e5846
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-10-23 16:01:24 +0200

    dependabot: make PRs for release-4.7 branch

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -exuo pipefail
+export OPENSHIFT_BUILD_REFERENCE=$(git rev-parse FETCH_HEAD)
+export FETCH_HEAD_CONTENT="$(cat ./.git/FETCH_HEAD)"
+export OPENSHIFT_BUILD_SOURCE="${FETCH_HEAD_CONTENT##* }"
+
+
+echo "Building ${OPENSHIFT_BUILD_REFERENCE} in ${OPENSHIFT_BUILD_SOURCE}"
+cirrus-run --github vrutkovs/okd-os --branch main --show-build-log always | tee /tmp/build.log
+
+INITIAL_IMAGE=$(grep "Committing container" -A1 /tmp/build.log | tail -n1 | tr ' ' '\n' | head -n1)
+sed "s;INITIAL_IMAGE;${INITIAL_IMAGE};g" Dockerfile.template > /tmp/Dockerfile
+
+OSTREE_VERSION="$(grep "OSTREE_VERSION" /tmp/build.log | cut -d"=" -f2)"
+sed -i "s;OSTREE_VERSION;${OSTREE_VERSION};g" /tmp/Dockerfile
+
+CRIO_RPM="$(grep "cri-o-" /tmp/build.log | head -n1 | cut -f 3 -d ' ')"
+sed -i "s;CRIO_RPM;${CRIO_RPM};g" /tmp/Dockerfile
+
+HYPERKUBE_RPM="$(grep "openshift-hyperkube-" /tmp/build.log | head -n1 | cut -f 3 -d ' ')"
+sed -i "s;HYPERKUBE_RPM;${HYPERKUBE_RPM};g" /tmp/Dockerfile
+
+mkdir ./extensions
