2792ae12f930d61352a2e21b3fd7db8257809bac
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-08-25 08:34:21 +0200

    Build OKD OS image on top of FCOS instead of rebuilding it

--- bootstrap/pre-pivot.sh
+++ bootstrap/pre-pivot.sh
@@ -7,24 +7,10 @@
 cp overlay/etc / -rvf
 cp manifests/* /opt/openshift/openshift/ -rvf
 
-# Copy machine-config-daemon binary from payload
-MACHINE_CONFIG_OPERATOR_IMAGE=$(image_for machine-config-operator)
-while ! podman pull --quiet "$MACHINE_CONFIG_OPERATOR_IMAGE"
-do
-    echo "Pull failed. Retrying $MACHINE_CONFIG_OPERATOR_IMAGE..."
-done
-mnt=$(podman image mount "${MACHINE_CONFIG_OPERATOR_IMAGE}")
-cp ${mnt}/usr/bin/machine-config-daemon /usr/local/bin/machine-config-daemon
-chmod +x /usr/local/bin/machine-config-daemon
-restorecon /usr/local/bin/machine-config-daemon
-
-# Set image-pullspec for pivot
-mkdir --parents /run/pivot /etc/pivot
+# Pivot to new os content
 MACHINE_CONFIG_OSCONTENT=$(image_for machine-os-content)
-echo "${MACHINE_CONFIG_OSCONTENT}" > /etc/pivot/image-pullspec
-touch /run/pivot/reboot-needed
-
+rpm-ostree rebase --experimental "ostree-unverified-registry:${MACHINE_CONFIG_OSCONTENT}"
+# Remove mitigations kargs
+rpm-ostree kargs --delete mitigations=auto,nosmt
 touch /opt/openshift/.pivot-done
-/usr/local/bin/machine-config-daemon pivot
-
 systemctl reboot
