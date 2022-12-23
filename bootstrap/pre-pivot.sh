195024f615b3fd6fbfb2db34e2077af614eefaf7
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-12-23 14:34:32 +0100

    pre-pivot: fetch "fedora-coreos" image
    This updates OKD OS image name to use "fedora-coreos" instead of deprecated "machine-os-content"

--- bootstrap/pre-pivot.sh
+++ bootstrap/pre-pivot.sh
@@ -9,9 +9,9 @@ cp overlay/etc / -rvf
 cp manifests/* /opt/openshift/openshift/ -rvf
 
 # Pivot to new os content
-MACHINE_CONFIG_OSCONTENT=$(image_for machine-os-content)
+MACHINE_OS_IMAGE=$(image_for fedora-coreos)
 stat /etc/containers/registries.conf
-rpm-ostree rebase --experimental "ostree-unverified-registry:${MACHINE_CONFIG_OSCONTENT}"
+rpm-ostree rebase --experimental "ostree-unverified-registry:${MACHINE_OS_IMAGE}"
 # Remove mitigations kargs
 rpm-ostree kargs --delete mitigations=auto,nosmt
 touch /opt/openshift/.pivot-done
