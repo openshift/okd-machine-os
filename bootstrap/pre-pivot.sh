a0179a1428f103aaa7fefac50a67a73e188c78dd
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-12-24 20:55:34 +0100

    pre-pivot: change registries.conf permission
    This makes sure rpm-ostreed can read registries.conf file so that
    rpm-ostree rebase via OCI containers

--- bootstrap/pre-pivot.sh
+++ bootstrap/pre-pivot.sh
@@ -9,9 +9,11 @@ cp overlay/etc / -rvf
 cp manifests/* /opt/openshift/openshift/ -rvf
 
 # Pivot to new os content
-MACHINE_CONFIG_OSCONTENT=$(image_for machine-os-content)
-stat /etc/containers/registries.conf
-rpm-ostree rebase --experimental "ostree-unverified-registry:${MACHINE_CONFIG_OSCONTENT}"
+MACHINE_OS_IMAGE=$(image_for fedora-coreos)
+# Make sure registries.conf is readable by rpm-ostree
+chmod 0644 /etc/containers/registries.conf
+rpm-ostree rebase --experimental "ostree-unverified-registry:${MACHINE_OS_IMAGE}"
+
 # Remove mitigations kargs
 rpm-ostree kargs --delete mitigations=auto,nosmt
 touch /opt/openshift/.pivot-done
